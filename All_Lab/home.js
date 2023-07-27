8faf6aea61c2fe61eeaf50faf8bc771b1f6bd895
NutThai <88415649+NutThai@users.noreply.github.com>
2023-07-27 12:33:50 +0700

    Update home.js

--- All_Lab/home.js
+++ All_Lab/home.js
@@ -8,23 +8,35 @@ import {
     View,
     FlatList
 } from "react-native";
-
 var all_lab = ["Lab2_1", "Lab2_2", "Lab3_1", "Lab3_2"]
+var header = [...new Set(all_lab.map(item => item.split('_')[0]))];
+
+console.log(header)
+// const lab_week = ({ week, assign }) => {
+//     return (
+//         <View>
+//             <Text>{ }</Text>
+//             <Button title={item} onPress={() => navigation.navigate(item)} />
+//         </View>
+//     )
+// }
 const Home = ({ navigation }) => {
 
     return (
         <View style={style1.container}>
-            <FlatList
+            <FlatList style={{ margin: "100" }}
                 data={all_lab}
-                renderItem={({ item }) =>
-                    <Button title={item} onPress={() => navigation.navigate(item)} />
+                keyExtractor={item => item}
+                renderItem={({ item }) => {
+                    return (
+                        <View style={style1.btn}>
+                            {/* <Text>{header}</Text> */}
+                            <Button title={item} onPress={() => navigation.navigate(item)} />
+                        </View>
+                    )
                 }
-                keyExtractor={item => item.major}
+                }
             />
-            {/* <Button title="Lab2_1" onPress={() => navigation.navigate('Lab2_1')} />
-            <Button title="Lab2_2" onPress={() => navigation.navigate('Lab2_2')} />
-            <Button title="Lab3_1" onPress={() => navigation.navigate('Lab3_1')} />
-            <Button title="Lab3_2" onPress={() => navigation.navigate('Lab3_2')} /> */}
         </View>
     );
 };
@@ -43,23 +55,14 @@ const style1 = StyleSheet.create({
     bot: {
         flex: 2,
     },
-    button: {
+    btn: {
         padding: 10,
-        width: 200,
+        width: 300,
         textAlign: "center",
         marginBottom: 5,
         fontSize: 20,
     },
-    logo: {
-        width: "25%",
-        height: undefined,
-        aspectRatio: 1,
-        resizeMode: "contain",
-    },
-    head: {
-        fontSize: 20,
-        marginBottom: 5,
-    },
+
 });
 export { Home };
 export { all_lab }
