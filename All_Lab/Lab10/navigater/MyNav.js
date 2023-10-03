e2af3e6038b966083184fef97cf5e10a6333f946
Nut Thaiwattananon <88415649+NutThai@users.noreply.github.com>
2023-10-03 14:28:42 +0700

    Update MyNav.js

--- All_Lab/Lab10/navigater/MyNav.js
+++ All_Lab/Lab10/navigater/MyNav.js
@@ -6,12 +6,12 @@ import StudentInfo from '../screen/StudentInfo'
 
 const mainNav = createStackNavigator()
 const MyNav = () => {
-    
+
   return (
     <mainNav.Navigator>
-        <mainNav.Screen name='AddStudent' component={AddStudent} />
-        <mainNav.Screen name='ViewStudent'component={ViewStudent} />
-        <mainNav.Screen name='StudentInfo'component={StudentInfo} />
+      <mainNav.Screen name='AddStudent' component={AddStudent} options={{ title: "Add Student", headerStyle: {backgroundColor:"#007eed"}}} />
+      <mainNav.Screen name='ViewStudent' component={ViewStudent} options={{ title: "Student List" , headerStyle: {backgroundColor:"#007eed"}}} />
+      <mainNav.Screen name='StudentInfo' component={StudentInfo} options={{ title: "Student Info" , headerStyle: {backgroundColor:"#007eed"}}} />
     </mainNav.Navigator>
   )
 }
